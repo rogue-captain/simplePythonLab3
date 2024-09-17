92c5384b0d3c65fbefc5b9042441edf220eaf615
Sully Erkan-Rijos <mr.suleyman.erkan@gmail.com>
2024-09-16 22:33:11 -0400

    finsihed block B

--- Erkan-RijosSuleymanLab3.py
+++ Erkan-RijosSuleymanLab3.py
@@ -10,14 +10,20 @@ Created on Sun Sep 15 20:27:33 2024
 
 # Program: Lab 3
 
-# Due Date: 09/xx/2024
+# Due Date: 09/27/2024
 #
-# Program Description:  
+# Program Description: This program will use a for and while loops to echo the numbers. 
+# the program will then use another for loop to print a chart with numbers from an array and
+# their respective vallue whenn squared then cubed.
+
+# The program will then use a while loop with a counter to iterate a grade collection 
+# prompt from user and print the letter grade 7 times. The program will then tell 
+# the user how many inputs there were, class grade point total and grade average.
 #
-# Inputs: 3 variables: name, num_Input[0,2](Array)
+# Inputs: name, 7 grades.
 #
-# Outputs: console calculates and prints users 3 number inputs, min, max, 
-#average, range and letter grade based on inputs from user. 
+# Outputs: 0-5 sequences using lopps, Table with numbers' 0 - 5 squares and cubes.
+#          class grades, total grade points and average grade. 
 #
 ##############################################################################
 
@@ -26,31 +32,36 @@ Created on Sun Sep 15 20:27:33 2024
 #array of integers
 nums = [0,1,2,3,4,5]
 
+#counters
 turnCounter = 0
 numbers = 0
 
 gradeCounter = 0
+
+gradeTotal = 0
 
 ################################ VARIABLES ####################################
 
 #A
 #Q3 2.8
 
+#for loop to iterate from 0-6
 for n in range(0,6):
-    print(n, end=' ')
+    print(n, end=' ') #creates a horizontal display
     
 print()#spacer
 print()#spacer
 
+#counter controlled while loop to iterate through numbers 0-5
 while turnCounter < 6:
     
-    print(numbers, end=' ')
-    numbers+=1
-    turnCounter+=1 
+    print(numbers, end=' ')#creates a horizontal display
+    numbers+=1 #increment
+    turnCounter+=1 #increment
     
     
-print()
-print()
+print()#spacer
+print()#spacer
 
 #header of the chart
 print(f"{'Integer':>10}\t\t{'Squared':>10}\t\t{'Cubed':>10}")
@@ -68,46 +79,75 @@ for n in nums:
 ###############################################################################
 ###############################################################################
 ###############################################################################
-print()
-print()
+
+print()#spacer
+print()#spacer
 
 #B
-name = input("enter your name please ") #collects string for user name
+name = input("enter your name please: ") #collects string for user name
 
-while gradeCounter < 5: #while loop to ensure checks are run and user re-prompted
+print()#spacer
+
+#counter controlled while loop to loop 7 times
+while gradeCounter < 7: #while loop to ensure checks are run and user re-prompted
     
   
     #this is a if else check to verify user has inputted a number
-    try:
-        gradeInput = int(input("Enter your grade. "))
+    try: #i used a TRY/EXEPTION block here to ensure user can only enter numbers
+        gradeInput = int(input("Enter your grade. "))#collects input
         
+        #check for unput range 0-100
         if not (0 <= gradeInput <= 100):
             print("ERROR: Enter grades ranging from 0-100 only.")
             
         else:
                     
                 print("you entered: ", gradeInput)
-    
                 
-    
+                gradeTotal = gradeTotal + gradeInput
+                
+                print()#spacer
                 
                 # Determine and print the letter grade based on the average
+                #acts like a select case or switch block but those dont exist 
+                #in pyhton apparently ?
+                
+                #Nested If Else
                 if gradeInput >= 60:
                     if gradeInput >= 90:
                         print(name, "Your grade is A")
+                        print()#spacer
                     elif gradeInput >= 80:
                         print(name, "Your grade is B")
+                        print()#spacer
                     elif gradeInput >= 70:
                         print(name, "Your grade is C")
+                        print()#spacer
                     else:
                         print(name, "Your grade is D")
+                        print()#spacer
                 else:
                     print(name, "Your grade is F, you have FAILED")
-
-                    
-                gradeCounter += 1
-                    
+                    print()#spacer
+                 
+                gradeCounter += 1#increment counter
+    
+    #except block to catch value error exception (letters)                
     except ValueError:
         print("ERROR: Enter grades ranging from 0-100 only.")
+        
+print()#spacer
+        
+print("Grade input counter: ", gradeCounter)#output how many grades were entered
+
+print()#spacer
+
+print("Total class grade points: ", gradeTotal)#sum of grades
+
+print()#spacer
+
+#output grade average
+print ("Your average grade is: {:.2f}".format(gradeTotal/gradeCounter))
+
                 
                 
